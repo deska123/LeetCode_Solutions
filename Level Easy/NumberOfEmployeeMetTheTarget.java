043ca2b95149a5907729f3cdbbc250e4f253e145
Dennis P Kamah <11691239+deska123@users.noreply.github.com>
2024-02-15 10:24:40 +0700

    Create NumberOfEmployeeMetTheTarget.java

---
+++ Level Easy/NumberOfEmployeeMetTheTarget.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int output = 0;
+        for(int i = 0; i < hours.length; i++) {
+            if(hours[i] >= target) output++;
+        }
+        return output;
+    }
+}
